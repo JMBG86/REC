55560e197feb25e12206cf8907fb986e9be5cd34
JMBG86 <46826624+JMBG86@users.noreply.github.com>
2025-08-24 15:38:11 +0100

    Adiciona __init__.py na pasta models para resolver o problema de importação

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,1 @@
-# Este arquivo permite que o Python reconheça este diretório como um pacote
+# Este arquivo permite que o Python reconheça este diretório como um pacote\n\n# Importar todos os modelos para facilitar a importação\nfrom src.models.car_model import CarBrand, CarModel\nfrom src.models.vehicle import Vehicle, VehicleUpdate, Document\nfrom src.models.rent_a_car import RentACar, EmailTrigger\nfrom src.models.store_location import StoreLocation\nfrom src.models.user import User, db
